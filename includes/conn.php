fffde71ff58cb611f3454c72341fbe8ffde3b6e9
Siraj Alam <Sirajalam049@gmail.com>
2020-06-16 01:14:44 +0530

    Pictures added to the data.json

--- includes/conn.php
+++ includes/conn.php
@@ -3,7 +3,7 @@
 function con()
 {
     $u = 'root';
-    $p = ''; // Your mysql password comes here.
+    $p = 'mysql-049'; // Your mysql password comes here.
     $d = 'hotels';
 
     $GLOBALS['con'] = new mysqli('127.0.0.1', $u, $p, $d)
